9773ab6eb9d81b7c682675ca67eda00064a83a41
Berenice982708 <61566117+Berenice982708@users.noreply.github.com>
2020-06-11 17:01:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 // Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
-app.use('/links', require('./routes/links'));
+
 
 
 // Public
